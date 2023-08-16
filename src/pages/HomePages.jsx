7769acc46a8a5dc87769fd7a8cd1731012665222
Javier Ospina <jaheosig10@gmail.com>
2023-08-15 23:32:13 -0500

    cambios en la imagen

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,15 +1,25 @@
+import "./style/HomePages.css"
+
 const HomePages = () => {
   return (
-    <div>
-      <h1>Javier Ospina </h1>
-      <h3>Desarrollador Full Stack</h3>
-      <p>
-        ¡Bienvenido a mi portafolio creativo! Me complace compartir contigo una
-        muestra de mi trabajo, logros y pasiones a lo largo de mi trayectoria
-        como programador. En estas páginas encontrarás una colección de
-        proyectos que representan mi compromiso con la excelencia, la
-        creatividad y la dedicación en cada paso del camino.  
-      </p>
+    <div className="portfolio">
+      <div className="portfolio__tit-des">
+        <h1 className="portfolio__title">Javier_Ospina</h1>
+        <h3 className="portfolio__subtitle">Desarrollador Full Stack</h3>
+        <p className="portfolio__description">
+          ¡Bienvenido a mi portafolio creativo! Me complace compartir contigo
+          una muestra de mi trabajo, logros y pasiones a lo largo de mi
+          trayectoria como programador. En estas páginas encontrarás una
+          colección de proyectos que representan mi compromiso con la
+          excelencia, la creatividad y la dedicación en cada paso del camino.
+        </p>
+      </div>
+        <img
+          className="portfolio__img"
+          src="../public/img/iconoprincipal.png"
+          alt=""
+        />
+      
     </div>
   )
 }
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,11 +3,11 @@
 const Skills = () => {
   return (
     <div>
-      <img src="../public/img/css3-3950d31a.svg" alt="" />
-      <img src="../public/img/html5-d4415476.svg" alt="" />
-      <img src="../public/img/javascript-a89d154c.svg" alt="" />
-      <img src="../public/img/react-56311bed.svg" alt="" />
-      <img src="../public/img/nodejs-9371c6ba.svg" alt="" />
+      <img src="../public/img/css33950d31a.svg" alt="" />
+      <img src="../public/img/html5d4415476.svg" alt="" />
+      <img src="../public/img/javascripta89d154c.svg" alt="" />
+      <img src="../public/img/react56311bed.svg" alt="" />
+      <img src="../public/img/nodejs9371c6ba.svg" alt="" />
     </div>
   )
 }
