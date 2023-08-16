8b8a2ab084e4fc0b2cbf6a97f7a4d49501d0c312
Javier Ospina <jaheosig10@gmail.com>
2023-08-16 17:03:10 -0500

    transformaciones

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -5,29 +5,17 @@ const Skills = () => {
     <div className="skills-container">
       <h1 className="skills-title">Habilidades</h1>
       <div className="skills">
-        <img className="skills__icon" src="/img/css33950d31a.svg" alt="" />
-        <img
-          className="skills__icon"
-          src="/img/html5d4415476.svg"
-          alt=""
-        />
-        <img
-          className="skills__icon"
-          src="/img/javascripta89d154c.svg"
-          alt=""
-        />
-        <img
-          className="skills__icon"
-          src="/img/react56311bed.svg"
-          alt=""
-        />
-        <img
-          className="skills__icon"
-          src="/img/nodejs9371c6ba.svg"
-          alt=""
-        />
+          <img className="skills__icon-css" src="/img/css33950d31a.svg" alt="" />
+          <img className="skills__icon-html" src="/img/html5d4415476.svg" alt="" />
+          <img
+            className="skills__icon-javascript"
+            src="/img/javascripta89d154c.svg"
+            alt=""
+          />
+          <img className="skills__icon-react" src="/img/react56311bed.svg" alt="" />
+          <img className="skills__icon-nodejs" src="/img/nodejs9371c6ba.svg" alt="" />
+        </div>
       </div>
-    </div>
   )
 }
 
