ba2a9929c1f3ba2486ee7b3fefef553d70c32704
Javier Ospina <jaheosig10@gmail.com>
2023-08-16 22:14:11 -0500

    port

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,121 @@
-import React from 'react'
+import "./style/Projects.css"
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="card-container">
+      <article className="card">
+        <div className="temporary_text">Place image here</div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">
+            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+          </span>
+          <p className="card_description">
+            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
+            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
+            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+          </p>
+        </div>
+      </article>
+      <article className="card">
+        <div className="temporary_text">Place image here</div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">
+            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+          </span>
+          <p className="card_description">
+            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
+            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
+            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+          </p>
+        </div>
+      </article>
+      <article className="card">
+        <div className="temporary_text">Place image here</div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">
+            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+          </span>
+          <p className="card_description">
+            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
+            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
+            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+          </p>
+        </div>
+      </article>
+      <article className="card">
+        <div className="temporary_text">Place image here</div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">
+            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+          </span>
+          <p className="card_description">
+            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
+            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
+            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+          </p>
+        </div>
+      </article>
+      <article className="card">
+        <div className="temporary_text">Place image here</div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">
+            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+          </span>
+          <p className="card_description">
+            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
+            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
+            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+          </p>
+        </div>
+      </article>
+      <article className="card">
+        <div className="temporary_text">Place image here</div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">
+            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+          </span>
+          <p className="card_description">
+            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
+            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
+            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+          </p>
+        </div>
+      </article>
+      <article className="card">
+        <div className="temporary_text">Place image here</div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">
+            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+          </span>
+          <p className="card_description">
+            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
+            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
+            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+          </p>
+        </div>
+      </article>
+      <article className="card">
+        <div className="temporary_text">Place image here</div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">
+            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+          </span>
+          <p className="card_description">
+            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
+            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
+            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+          </p>
+        </div>
+      </article>
+    </div>
   )
 }
 
