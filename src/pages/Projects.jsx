f250a345486231b13dc3942f573745ab53aeaa21
Javier Ospina <jaheosig10@gmail.com>
2023-08-17 11:45:51 -0500

    Proyectos realizados

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,44 @@ const Projects = () => {
   return (
     <div className="card-container">
       <article className="card">
-        <div className="temporary_text"><img className='card-img' src="/img/apiclima.png" alt="" /></div>
+        <div className="temporary_text">
+          <img className="card-img" src="/img/ecommercevanilla.png" alt="" />
+        </div>
+        <div className="card_content">
+          <span className="card_title">Shop - AJ</span>
+          <span className="card_subtitle">ecommerce de ropa</span>
+          <p className="card_description">
+            "Diseño e-commerce ropa con API
+            ('https://ecommercebackend.fundamentos-29.repl.co'). Carrito, modo
+            oscuro y detalles de productos para una experiencia de compra
+            completa y mejorada. Busca mostrar al usuario de forma clara cada
+            producto "
+          </p>
+        </div>
+      </article>
+
+      <article className="card">
+        <div className="temporary_text">
+          <img className="card-img" src="/img/apigalleta.png" alt="" />
+        </div>
+        <div className="card_content">
+          <span className="card_title">Galletica</span>
+          <span className="card_subtitle">
+            juega a la galleta de la fortuna
+          </span>
+          <p className="card_description">
+            "Sitio web 'Galletica de la Fortuna': Simulación virtual de abrir
+            galletas de la fortuna con mensajes inspiradores y consejos en
+            línea. Experiencia interactiva y entretenida. Usando un JSON que
+            proporciona las frases, con cambio de fondo".
+          </p>
+        </div>
+      </article>
+
+      <article className="card">
+        <div className="temporary_text">
+          <img className="card-img" src="/img/apiclima.png" alt="" />
+        </div>
         <div className="card_content">
           <span className="card_title">Climatic</span>
           <span className="card_subtitle">aplicacion de clima</span>
@@ -18,100 +55,70 @@ const Projects = () => {
       </article>
 
       <article className="card">
-        <div className="temporary_text">Place image here</div>
+        <div className="temporary_text">
+          <img className="card-img" src="/img/apirickandmorthy.png" alt="" />
+        </div>
         <div className="card_content">
-          <span className="card_title">This is a Title</span>
-          <span className="card_subtitle">
-            Thsi is a subtitle of this page. Let us see how it looks on the Web.
-          </span>
+          <span className="card_title">Rick and Morty App</span>
+          <span className="card_subtitle">personajes de rick and morty.</span>
           <p className="card_description">
-            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+            "Esta aplicación utiliza la API de
+            'https://rickandmortyapi.com/documentation/#get-a-single-location'
+            para buscar personajes en diferentes dimensiones. Permite ingresar
+            un número de dimensión o buscar aleatoriamente mediante un botón."
           </p>
         </div>
       </article>
+
       <article className="card">
-        <div className="temporary_text">Place image here</div>
+        <div className="temporary_text">
+          <img className="card-img" src="/img/usercrud.png" alt="" />
+        </div>
         <div className="card_content">
-          <span className="card_title">This is a Title</span>
-          <span className="card_subtitle">
-            Thsi is a subtitle of this page. Let us see how it looks on the Web.
-          </span>
+          <span className="card_title">User Crud</span>
+          <span className="card_subtitle">Pagina creadora de usuarios</span>
           <p className="card_description">
-            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+            "Diseño de página web para crear usuarios con la API
+            https://users-crud.academlo.tech/swagger/. Emplea el método CRUD,
+            presentando formulario de datos y modales informativos en cada
+            proceso, la aplicacion tiene el modo oscuro."
           </p>
         </div>
       </article>
+
       <article className="card">
-        <div className="temporary_text">Place image here</div>
+        <div className="temporary_text">
+          {" "}
+          <img className="card-img" src="/img/pokeapi.png" alt="" />
+        </div>
         <div className="card_content">
-          <span className="card_title">This is a Title</span>
+          <span className="card_title">Pokedex app</span>
           <span className="card_subtitle">
-            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+            Aplicacion que muestra los pokemons
           </span>
           <p className="card_description">
-            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+            La pagina muestra los pokemones utilizando la API https://pokeapi.co
+            con la cual podemos hacer diferentes tipos de filtrado, utilizando
+            las rutas que permiten el acceso a informacion detallada de cada
+            pokemon contiene un filtro por nombre y por tipo.
           </p>
         </div>
       </article>
+
       <article className="card">
-        <div className="temporary_text">Place image here</div>
-        <div className="card_content">
-          <span className="card_title">This is a Title</span>
-          <span className="card_subtitle">
-            Thsi is a subtitle of this page. Let us see how it looks on the Web.
-          </span>
-          <p className="card_description">
-            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-            repudiandae aspernatur? Labore minus soluta consequatur placeat.
-          </p>
+        <div className="temporary_text">
+          <img className="card-img" src="/img/ecommercetecno.png" alt="" />
         </div>
-      </article>
-      <article className="card">
-        <div className="temporary_text">Place image here</div>
         <div className="card_content">
-          <span className="card_title">This is a Title</span>
+          <span className="card_title">AJ tecnology</span>
           <span className="card_subtitle">
-            Thsi is a subtitle of this page. Let us see how it looks on the Web.
+            ecommerce de productos tecnologia
           </span>
           <p className="card_description">
-            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-            repudiandae aspernatur? Labore minus soluta consequatur placeat.
-          </p>
-        </div>
-      </article>
-      <article className="card">
-        <div className="temporary_text">Place image here</div>
-        <div className="card_content">
-          <span className="card_title">This is a Title</span>
-          <span className="card_subtitle">
-            Thsi is a subtitle of this page. Let us see how it looks on the Web.
-          </span>
-          <p className="card_description">
-            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-            repudiandae aspernatur? Labore minus soluta consequatur placeat.
-          </p>
-        </div>
-      </article>
-      <article className="card">
-        <div className="temporary_text">Place image here</div>
-        <div className="card_content">
-          <span className="card_title">This is a Title</span>
-          <span className="card_subtitle">
-            Thsi is a subtitle of this page. Let us see how it looks on the Web.
-          </span>
-          <p className="card_description">
-            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+            "Aplicación de comercio electrónico con integración API
+            https://documenter.getpostman.com/view/17877993/2s8ZDczLBj. Muestra
+            y filtra productos tecnológicos por precio y tipo. Incluye registro
+            de usuarios, carrito de compras y historial de compras."
           </p>
         </div>
       </article>
