1119e9a6f765468741792444083cce27397bb0fb
Javier Ospina <jaheosig10@gmail.com>
2023-08-17 10:37:04 -0500

    ajustes de imagenes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,19 +4,19 @@ const Projects = () => {
   return (
     <div className="card-container">
       <article className="card">
-        <div className="temporary_text">Place image here</div>
+        <div className="temporary_text"><img className='card-img' src="/img/apiclima.png" alt="" /></div>
         <div className="card_content">
-          <span className="card_title">This is a Title</span>
-          <span className="card_subtitle">
-            Thsi is a subtitle of this page. Let us see how it looks on the Web.
-          </span>
+          <span className="card_title">Climatic</span>
+          <span className="card_subtitle">aplicacion de clima</span>
           <p className="card_description">
-            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+            "Aplicación climática con API global ('https://openweathermap.org/')
+            que ofrece búsqueda por ubicación, modo oscuro, conversión °C a °F y
+            detalles como presión, nubes y viento. Muestra temperatura y estado
+            de nubes."
           </p>
         </div>
       </article>
+
       <article className="card">
         <div className="temporary_text">Place image here</div>
         <div className="card_content">
