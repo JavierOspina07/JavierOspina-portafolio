1c12167c7ee567e22b5135afc4163fc854f6500b
Javier Ospina <jaheosig10@gmail.com>
2023-08-17 23:45:18 -0500

    mejoras del darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,13 @@ import Contact from "./pages/Contact"
 import Projects from "./pages/Projects"
 import Skills from "./pages/Skills"
 import SocialNetworks from "./components/shared/SocialNetworks"
-import useDarkMode from "./components/useDarkMode"
+import DarkModebutton from "./components/DarkModebutton"
 
 function App() {
-  /* Darkmode */
-  const [darkMode, setDarkMode] = useDarkMode()
-
-  /* Manipulacion para Darkmode */
-  const handleToggleDarkMode = () => {
-    setDarkMode(prevDarkMode => !prevDarkMode)
-  }
-
   return (
     <div className="app">
       <Header />
-      <div className="wrapper">
-        <input
-          type="checkbox"
-          id="hide-checkbox"
-          defaultChecked={!darkMode}
-          onChange={handleToggleDarkMode}
-        />
-        <label htmlFor="hide-checkbox" className="toggle">
-          <span className="toggle-button">
-            <span className="crater crater-1"></span>
-            <span className="crater crater-2"></span>
-            <span className="crater crater-3"></span>
-            <span className="crater crater-4"></span>
-            <span className="crater crater-5"></span>
-            <span className="crater crater-6"></span>
-            <span className="crater crater-7"></span>
-          </span>
-          <span className="star star-1"></span>
-          <span className="star star-2"></span>
-          <span className="star star-3"></span>
-          <span className="star star-4"></span>
-          <span className="star star-5"></span>
-          <span className="star star-6"></span>
-          <span className="star star-7"></span>
-          <span className="star star-8"></span>
-        </label>
-      </div>
+      <DarkModebutton/>
       <SocialNetworks />
       <Routes>
         <Route path="/" element={<HomePages />} />
