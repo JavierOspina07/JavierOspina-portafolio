e085fa3e3611b0aa6d12dcfcf3498c9c815f2c30
Javier Ospina <jaheosig10@gmail.com>
2023-08-23 16:54:18 -0500

    cambio de idioma

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import "./style/Header.css"
 import { useState } from "react"
+import {useTranslation} from 'react-i18next'
 
 const header = () => {
   //estado menu oculto
@@ -9,6 +10,13 @@ const header = () => {
   const handleMobile = () => {
     isMobile ? setIsMobile(false) : setIsMobile(true)
   }
+  const [t, i18n] = useTranslation("global")
+
+
+  const handleLanguaje =() =>{
+   i18n.changeLanguage(i18n.language === 'es' ? 'en' : 'es')
+  }
+
 
   return (
     <div className="header-container">
@@ -21,6 +29,7 @@ const header = () => {
               className="header__logo-image"
             />
             <h2 className="header__logo-title">Javier Ospina</h2>
+            <button onClick={handleLanguaje}><i class="fa-solid fa-language"></i></button>
           </div>
         </Link>
         <nav className="header__nav">
@@ -32,27 +41,27 @@ const header = () => {
           >
             <li className="header__nav-item">
               <Link to="/" className="header__nav-link">
-                Inicio
+                {t("Header.Home")}
               </Link>
             </li>
             <li className="header__nav-item">
               <Link to="/about" className="header__nav-link">
-                Acerca De
+              {t("Header.About")}
               </Link>
             </li>
             <li className="header__nav-item">
               <Link to="/skills" className="header__nav-link">
-                Habilidades
+              {t("Header.Skills")}
               </Link>
             </li>
             <li className="header__nav-item">
               <Link to="/projects" className="header__nav-link">
-                Proyectos
+              {t("Header.Projects")}
               </Link>
             </li>
             <li className="header__nav-item">
               <Link to="/contact" className="header__nav-link">
-                Contacto
+              {t("Header.Contact")}
               </Link>
             </li>
           </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,33 @@ import App from "./App.jsx"
 import "./index.css"
 import { HashRouter } from "react-router-dom"
 import { Provider } from "react-redux"
-import store from './store/index.js'
+import store from "./store/index.js"
+import { I18nextProvider } from "react-i18next"
+import i18next from "i18next"
+import global_es from "../src/lang/es/global.json"
+import global_en from "../src/lang/en/global.json"
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
+    <I18nextProvider i18n={i18next}>
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
+    </I18nextProvider>
   </React.StrictMode>
 )
