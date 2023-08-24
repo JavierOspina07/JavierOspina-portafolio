3ec253fcf6dec25aaafb4df0122479de0063908c
Javier Ospina <jaheosig10@gmail.com>
2023-08-23 23:15:18 -0500

    traduccion de la pagina

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './style/Skills.css';
+import { useTranslation } from "react-i18next"
+
 
 const Skills = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [t, i18n] = useTranslation("global")
+
 
   useEffect(() => {
     setIsVisible(true);
@@ -10,7 +14,7 @@ const Skills = () => {
 
   return (
     <div className="skills-container">
-      <h1 className="skills-title">Habilidades</h1>
+      <h1 className="skills-title">{t("Skills.skills")}</h1>
       <div className="skills">
         <img className={`skills__icon skills__icon-css ${isVisible ? 'appear' : ''}`} src="/img/css-01.svg" alt="" />
         <img className={`skills__icon skills__icon-html ${isVisible ? 'appear' : ''}`} src="/img/html-01.svg" alt="" />
